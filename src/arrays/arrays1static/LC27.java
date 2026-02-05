9cf1138063eec5145dba29567fb8ee8c2b4eae5e
Nikhil Kumar Goutham Kumar <nikhilkumar01g@gmail.com>
2026-02-04 20:31:00 -0500

    LC27: Remove all occurences of a value from the input array, modify array with values not equal to 'remove value' in the beginning of the array, return number of values that are not equal to the 'remove value'

--- src/arrays/arrays1static/LC27.java
+++ src/arrays/arrays1static/LC27.java
@@ -1,4 +1,23 @@
 package arrays.arrays1static;
 
+import java.util.Arrays;
+
 public class LC27 {
+
+    // Given an input array, perform this using in-place algorithm:
+    // 1. Remove value 'x' from the array.
+    // 2. The values not equal to 'x' should be in the beginning of the array, the rest of the values in the array don't matter.
+    // 3. Return the number of values not equal to value 'x'.
+
+    // Solution: Using two pointer
+    private int removeElement(int[] arr, int x) {
+        int writerPointer = 0;
+        for(int readerPointer = 0; readerPointer < arr.length; readerPointer++) {
+            if (arr[readerPointer] == x) continue;
+            arr[writerPointer++] = arr[readerPointer];
+        }
+        return writerPointer;
+    }
+
+
 }
